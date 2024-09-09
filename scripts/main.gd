29f19dd1557745963d238f8738060ed7e26d4597
Mark Mozarovski <mark.mozh@gmail.com>
2024-09-09 02:18:25 +0200

    sync blink with env change

--- scripts/main.gd
+++ scripts/main.gd
@@ -8,39 +8,36 @@ var peace_env = load("res://environments/peace_env.tres")
 @onready var blink_canvas: CanvasLayer = $BlinkCanvas
 
 var blink_counter: int = 0
-var blinks_to_storm: int = 3
+var blinks_to_storm: int = 1
 
 func _ready():
-	$BlinkCanvas.get_node("AnimationPlayer").animation_finished.connect(_on_anim_finished)
 	blink_timer.wait_time = randf_range(15.0, 20.0)
 
 func _process(_delta):
 	pass
 
 func _on_blink_timer_timeout():
-	blink_timer.wait_time = randf_range(5.0, 8.0)
 	blink_timer.wait_time = randf_range(15.0, 20.0)
+		
+	if blink_counter < blinks_to_storm:
+		blink_counter += 1
+	else:
+		blink_counter = 0
+		if is_storm:
+			storm_end()
+		else:
+			storm_start()
+			
 	blink_canvas.blink()
 	
 func storm_start():
+	await get_tree().create_timer(0.10).timeout
 	$WorldEnvironment.environment = storm_env
 	$WorldEnvironment/DirectionalLight3D.light_energy = 0.75
 	is_storm = true
 	
 func storm_end():
+	await get_tree().create_timer(0.10).timeout
 	$WorldEnvironment.environment = peace_env
 	$WorldEnvironment/DirectionalLight3D.light_energy = 1.0
 	is_storm = false
-	
-func _on_anim_finished(anim_name: String):
-	if anim_name == "blink":
-		if blink_counter < blinks_to_storm:
-			blink_counter += 1
-		else:
-			blink_counter = 0
-			if is_storm:
-				storm_end()
-			else:
-				storm_start()
-		
-		
