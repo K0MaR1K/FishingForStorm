1b07ceb55612c734afb51bcfc8645ba2596725c1
Timur-khan96 <tim-valiulin@yandex.ru>
2024-09-10 05:09:10 +0500

    weather update

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,6 +5,8 @@ extends Node3D
 var is_storm: bool = false
 var storm_env = load("res://environments/storm_env.tres")
 var peace_env = load("res://environments/peace_env.tres")
+var storm_effects_scene = load("res://scenes/storm_effects.tscn")
+var storm_node;
 @onready var blink_canvas: CanvasLayer = $BlinkCanvas
 
 var blink_counter: int = 0
@@ -34,10 +36,15 @@ func storm_start():
 	await get_tree().create_timer(0.10).timeout
 	$WorldEnvironment.environment = storm_env
 	$WorldEnvironment/DirectionalLight3D.light_energy = 0.75
+	storm_node = storm_effects_scene.instantiate()
+	add_child(storm_node)
+	$Player.get_node("rain").show()
 	is_storm = true
 	
 func storm_end():
 	await get_tree().create_timer(0.10).timeout
 	$WorldEnvironment.environment = peace_env
 	$WorldEnvironment/DirectionalLight3D.light_energy = 1.0
+	storm_node.queue_free()
+	$Player.get_node("rain").hide()
 	is_storm = false
