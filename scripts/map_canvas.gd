d228a454220bf372b5db8b1c526249311d8c3a72
Mark Mozarovski <mark.mozh@gmail.com>
2024-09-13 01:33:56 +0200

    add map canvas

---
+++ scripts/map_canvas.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+func _on_stormbreaker_button_pressed() -> void:
+	print("STORMBREAKER")
+
+
+func _on_seawitch_button_pressed() -> void:
+	print("SEAWITCH")
+
+
+func _on_buccaneer_button_pressed() -> void:
+	print("BUCCANEER")
+
+
+func _on_deadman_button_pressed() -> void:
+	print("DEADMAN")
